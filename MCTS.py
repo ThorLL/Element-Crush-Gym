c10c95b3678492cdefbb3fd4ef515135bdecdb60
ThorLL <thorlind030601@gmail.com>
2024-11-11 12:39:01 +0100

    MCTS

---
+++ MCTS.py
@@ -0,0 +1,170 @@
+import math
+import random
+import time
+
+from tqdm import tqdm
+
+from match3tile.board import Board
+import numpy as np
+
+
+class Node:
+    def __init__(self, state, moves, game_score, parent=None, config=(0, 6)):
+        self.state = np.copy(state)
+        self.parent: Node = parent
+        self.children = []
+        self.visits = 0
+        self.reward = 0
+        self.moves_left = moves
+        self.game_score = game_score
+        self.seed, self.types = config
+        self.actions = []
+
+    def best_child(self, c):
+        return max(self.children, key=lambda child: child.ucb1(c))
+
+    def step(self, action, deterministic=False):
+        if deterministic:
+            np.random.seed(self.seed)
+        else:
+            np.random.seed(random.randint(0, 2**32 - 1))
+        self.state = Board.swap_tokens(action, self.state)
+
+        tokens_matched = 0
+        chain_matches = 0
+
+        actions = []
+        while True:
+            matches = Board.get_matches(self.state)
+            if len(matches) == 0 and len(actions) == 0:
+                pass
+            if len(matches) == 0:
+                break
+            chain_matches += len(matches)
+            tokens_matched += sum([len(match) for match in matches])
+            self.state = Board.remove_matches(matches, self.state)
+            self.state = Board.drop(self.state)
+
+            while True:
+                safe_cpy = np.copy(self.state)
+                zero_mask = (self.state == 0)
+                random_values = np.random.randint(1, self.types + 1, size=self.state.shape, dtype=np.int32)
+                self.state[zero_mask] = random_values[zero_mask]
+                self.actions = Board.valid_actions(self.state)
+                if len(self.actions) > 0:
+                    break
+                else:
+                    self.state = safe_cpy
+        self.game_score += tokens_matched + tokens_matched * (chain_matches - 1)
+
+    def ucb1(self, c) -> float:
+        if self.visits == 0:
+            return float('inf')
+        return self.reward / self.visits + c * math.sqrt(math.log(self.parent.visits / self.visits))
+
+    def expand(self):
+        for action in self.actions:
+            child_node = Node(self.state, self.moves_left-1, self.game_score, self, (self.seed, self.types))
+            child_node.step(action, True)
+            self.children.append(child_node)
+
+    def __del__(self):
+        for i in range(len(self.children)):
+            del self.children[0]
+        del self.state
+        del self.parent
+        del self
+
+
+class MCTS:
+    def __init__(self, env, simulations=100):
+        self.root = Node(
+            state=np.copy(env.board.array),
+            moves=env.num_moves - env.moves_taken,
+            game_score=env.score,
+            config=(env.seed, env.num_types)
+        )
+        self.root.actions = env.board.actions
+
+        self.simulations = simulations
+
+        self.goal = env.env_goal
+        self.root.expand()
+
+        self.times = {
+            'tree traversal ': 0,
+            'expand         ': 0,
+            'rollout        ': 0,
+            'backpropagation': 0,
+        }
+
+        del env
+
+    def print_times(self):
+        print('MCTS TIME TABLE')
+        print('      Step      - Total - Avg pr call')
+        for name, v in self.times.items():
+            avg = self.times[name] / self.simulations
+            v = "%.2f" % v
+            if len(v) == 4:
+                v = '0'+v
+            print(name, v, "%.2f" % avg, sep=' - ')
+
+    def __call__(self):
+        for _ in tqdm(range(self.simulations)):
+            node = self.tree_traversal(self.root)
+            if node.visits > 0:
+                begin = time.time()
+                node.expand()
+                node = node.children[0]
+                self.times['expand         '] += time.time() - begin
+            reward = self.rollout(node)
+            self.backpropagation(node, reward)
+
+        next_root = self.root.best_child(0)
+        next_root_idx = self.root.children.index(next_root)
+        action = self.root.actions[next_root_idx]
+        next_root.parent = None
+        # clean up and prep for next call
+        self.root.children.remove(next_root)
+        del self.root
+        self.root = next_root
+
+        return action
+
+    def tree_traversal(self, node: Node):
+        begin = time.time()
+        while len(node.children) != 0:
+            node = node.best_child(3)
+        self.times['tree traversal '] += time.time() - begin
+        return node
+
+    def rollout(self, node):
+        begin = time.time()
+
+        node_state = np.copy(node.state)
+        node_actions = node.actions
+        node_score = node.game_score
+
+        for i in range(node.moves_left):  # Limit the depth of simulation
+            node.step(random.choice(node.actions))
+
+        if node.game_score >= self.goal:
+            diff = node.game_score - self.goal
+            c = 5
+            node.game_score += diff ** c
+
+        reward = node.game_score - node_score
+        node.state = node_state
+        node.actions = node_actions
+        node.game_score = node_score
+        self.times['rollout        '] += time.time() - begin
+        return reward
+
+    def backpropagation(self, node, reward):
+        begin = time.time()
+        while node:
+            node.visits += 1
+            node.reward += reward
+            node = node.parent
+        self.times['backpropagation'] += time.time() - begin
--- main.py
+++ main.py
@@ -1,9 +1,63 @@
+from MCTS import MCTS
 from match3tile.env import Match3Env
 from model import Model
 from trainingDataGen import get_train_and_test_data
 
 
-if __name__ == '__main__':
+def mcts(moves, goal):
+    mcts_env = Match3Env(num_moves=moves, env_goal=goal)
+    best_env = Match3Env(num_moves=moves, env_goal=goal)
+    naive_env = Match3Env(num_moves=moves, env_goal=goal)
+    random_env = Match3Env(num_moves=moves, env_goal=goal)
+
+    mcts_score = [0, 0]
+    best_move_score = 0
+    naive_score = 0
+    random_score = 0
+    done, won = False, False
+
+    mcts_algo = MCTS(mcts_env)
+
+    won_at = 0
+
+    while mcts_env.moves_taken != mcts_env.num_moves:
+        mcts_action, required_next_state = mcts_algo()
+
+        best_action = best_env.board.best_action()
+        naive_action = naive_env.board.naive_action()
+        random_action = random_env.board.random_action()
+
+        idx = 1 if won else 0
+        _, reward, done, won, _ = mcts_env.step(mcts_action)
+        mcts_score[idx] += reward
+
+        _, reward, _, _, _ = best_env.step(best_action)
+        best_move_score += reward
+
+        _, reward, _, _, _ = naive_env.step(naive_action)
+        naive_score += reward
+
+        _, reward, _, _, _ = random_env.step(random_action)
+        random_score += reward
+
+        if not won:
+            won_at += 1
+    if done:
+        if won:
+            print('Won game after', won_at, 'moves')
+        else:
+            print('Lost game')
+        print('mcts actions score:', mcts_score, sum(mcts_score))
+        print('best actions score:', best_move_score)
+        print('naive actions score:', naive_score)
+        print('random actions score:', random_score)
+        print('')
+
+        mcts_algo.print_times()
+    return mcts_score, best_move_score, naive_score, random_score
+
+
+def train_model():
     env = Match3Env()
 
     height, width, channels = env.observation_space
@@ -15,5 +69,11 @@ if __name__ == '__main__':
         learning_rate=0.005,
         momentum=0.9,
     )
+
     train_ds, test_ds = get_train_and_test_data()
     model.fit(train_ds, test_ds)
+
+
+if __name__ == '__main__':
+    mcts(20, 1000)
+
