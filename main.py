5379f72214f4b00760e32b5de51ea9f88082d6b1
ThorLL <thorlind030601@gmail.com>
2024-11-25 08:23:13 +0100

    testing main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from MCTS import MCTS
 from match3tile.env import Match3Env
 from model import Model
@@ -5,31 +7,47 @@ from trainingDataGen import get_train_and_test_data
 
 
 def mcts(moves, goal):
+    random_mcts_env = Match3Env(num_moves=moves, env_goal=goal)
     mcts_env = Match3Env(num_moves=moves, env_goal=goal)
     best_env = Match3Env(num_moves=moves, env_goal=goal)
     naive_env = Match3Env(num_moves=moves, env_goal=goal)
     random_env = Match3Env(num_moves=moves, env_goal=goal)
 
     mcts_score = [0, 0]
+    random_mcts_score = [0, 0]
     best_move_score = 0
     naive_score = 0
     random_score = 0
-    done, won = False, False
+    won = False
+    random_won = False
 
     mcts_algo = MCTS(mcts_env)
+    random_mcts = MCTS(random_mcts_env)
 
-    won_at = 0
+    won_at = 1
+    random_won_at = 1
+
+    mcts_actions = []
 
     while mcts_env.moves_taken != mcts_env.num_moves:
-        mcts_action, required_next_state = mcts_algo()
+
+        mcts_action = mcts_algo()
 
         best_action = best_env.board.best_action()
         naive_action = naive_env.board.naive_action()
         random_action = random_env.board.random_action()
 
         idx = 1 if won else 0
+
         _, reward, done, won, _ = mcts_env.step(mcts_action)
         mcts_score[idx] += reward
+
+        idx = 1 if random_won else 0
+
+        teh_action = random_mcts()
+        mcts_actions.append(teh_action)
+        _, reward, done, random_won, _ = random_mcts_env.step(teh_action)
+        random_mcts_score[idx] += reward
 
         _, reward, _, _, _ = best_env.step(best_action)
         best_move_score += reward
@@ -42,18 +60,37 @@ def mcts(moves, goal):
 
         if not won:
             won_at += 1
-    if done:
-        if won:
-            print('Won game after', won_at, 'moves')
-        else:
-            print('Lost game')
-        print('mcts actions score:', mcts_score, sum(mcts_score))
-        print('best actions score:', best_move_score)
-        print('naive actions score:', naive_score)
-        print('random actions score:', random_score)
-        print('')
+        if not random_won:
+            random_won_at += 1
 
-        mcts_algo.print_times()
+    if won:
+        print('Won game after', won_at, 'moves')
+    else:
+        print('Lost game')
+    if random_won:
+        print('Non deterministic Won game after', random_won_at, 'moves')
+    else:
+        print('Lost game')
+    print('mcts actions score:', mcts_score, sum(mcts_score))
+    print('non deterministic mcts actions score:', random_mcts_score, sum(random_mcts_score))
+    print('best actions score:', best_move_score)
+    print('naive actions score:', naive_score)
+    print('random actions score:', random_score)
+    print('')
+    mcts_algo.print_times()
+    random_mcts.print_times()
+
+    render_env = Match3Env(num_moves=moves, env_goal=goal, render_mode='human')
+    score = 0
+    for i, action in enumerate(mcts_actions):
+        print("Move:", i+1)
+        _, reward, _, _, _ = render_env.step(action)
+        score += reward
+        print(reward, score)
+        render_env.render()
+        sleep(0.5)
+
+
     return mcts_score, best_move_score, naive_score, random_score
 
 
@@ -75,5 +112,7 @@ def train_model():
 
 
 if __name__ == '__main__':
+    import os
+    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
     mcts(20, 1000)
 
