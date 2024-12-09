efbc1228463dcd607ebb059ce47617b28e2d7da3
GustavSO <guol@itu.dk>
2024-12-09 09:32:58 +0100

    Cleanup main + added more args for profiling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
 import time
+
+from numpy import full
 from MCTS import MCTS
 from elementGO.MCTSModel import Model
 from match3tile.env import Match3Env
@@ -13,6 +14,7 @@ import os
 import argparse
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
 
 def random_task():
     env = Match3Env()
@@ -78,28 +80,44 @@ def train_model():
 
 
 def override_pstats_prints():
-        """
-        By default, pstats only prints 3 decimal places. This function overrides the print functions to print 6 decimal places and slightly adjust the title position.
-        Should probably be upgraded to use Tabulate or something similar.
-        """
-        def f8_alt(x):
-            return "%9.6f" % x
+    """
+    By default, pstats only prints 3 decimal places. This function overrides the print functions to print 6 decimal places and slightly adjust the title position.
+    Should probably be upgraded to use Tabulate or something similar.
+    """
 
-        def print_title(self):
-            print('   ncalls   tottime   percall   cumtime   percall', end=' ', file=self.stream)
-            print('filename:lineno(function)', file=self.stream)
-        pstats.f8 = f8_alt
-        pstats.Stats.print_title = print_title
+    def f8_alt(x):
+        return "%9.6f" % x
+
+    def print_title(self):
+        print(
+            "   ncalls   tottime   percall   cumtime   percall",
+            end=" ",
+            file=self.stream,
+        )
+        print("filename:lineno(function)", file=self.stream)
+
+    pstats.f8 = f8_alt
+    pstats.Stats.print_title = print_title
+
 
 # Using seed 100
 # Time before refactoring: 20.025s
-def perform_profiling():
-    SEED = 100
-    RUN_PROFILER = True
+def perform_profiling(mode="full", sort_key="time", seed=100):
+    """
+    Runs the profiler on the MCTS algorithm and prints the results.
+    mode: "full" or "quick".
+            "quick" will only look at the MCTS.prof file (provided it exists)
+            "full" will run the profiler and save the results to MCTS.prof
+    sort_key: "calls", "cumtime", "time". Sorts the profiler output by the specified key.
+    """
 
-    env = Match3Env(seed=SEED)
+    print(f"Running profiler: \n - Mode: {mode} \n - Sort key: {sort_key} \n - Seed: {seed}")
+    print("-" * 50)
+
+    env = Match3Env(seed=seed)
     mcts = MCTS(env, 100, True)
-    if not os.path.exists("mcts.prof") or RUN_PROFILER:
+
+    if mode == "full" or not os.path.exists("mcts.prof"):
         profiler = cProfile.Profile()
         profiler.runctx("mcts.__call__()", globals(), locals())
         profiler.dump_stats("mcts.prof")
@@ -115,9 +133,9 @@ def perform_profiling():
         for key, value in p.stats.items()
         if any(file in key[0] for file in included_files)
     }
-    
-    p.strip_dirs().sort_stats(SortKey.TIME).print_stats()
 
+    print()
+    p.strip_dirs().sort_stats(sort_key).print_stats()
 
 
 def mcts_samples():
@@ -125,8 +143,8 @@ def mcts_samples():
     seeds = list(range(50, 1001, 50))
     rewards = []
 
-    move_count = 20 # 20 default
-    goal = 500      # 500 default
+    move_count = 20  # 20 default
+    goal = 500  # 500 default
 
     start_time = time.time()
 
@@ -148,7 +166,7 @@ def mcts_samples():
         print(f" - Total reward: {total_reward}")
         print(f" - MCTS moves: {mcts_moves}")
         print()
-    
+
     print("-" * 50)
     print("Results from running MCTS sample (optimized branch)")
     print(f" - Sample count: {len(seeds)}")
@@ -158,14 +176,16 @@ def mcts_samples():
     print(f" - Average reward: {sum(rewards) / len(rewards)}")
 
 
-def run_mcts(seed=100, move_count=20, goal=500, render=False):
+def mcts_single(seed=100, move_count=20, goal=500, render=False):
     env = Match3Env(seed=seed, num_moves=move_count, env_goal=goal)
     mcts = MCTS(env, 100, False)
 
     mcts_moves = []
     total_reward = 0
-    
-    print(f"Performing \"optimized\" MCTS (seed: {seed}, moves: {move_count}, goal: {goal})")
+
+    print(
+        f'Performing "optimized" MCTS (seed: {seed}, moves: {move_count}, goal: {goal})'
+    )
     print("-" * 50)
     start_time = time.time()
     while env.moves_taken != env.num_moves:
@@ -179,18 +199,43 @@ def run_mcts(seed=100, move_count=20, goal=500, render=False):
     print(f"MCTS moves: {mcts_moves}")
 
     if render:
-        env_copy = Match3Env(seed=seed, num_moves=move_count, env_goal=goal, render_mode="human")
+        env_copy = Match3Env(
+            seed=seed, num_moves=move_count, env_goal=goal, render_mode="human"
+        )
         for move in mcts_moves:
             env_copy.step(move)
             env_copy.render()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--profile", action="store_true", default=False, help="Run the profiler")
+    parser.add_argument(
+        "--profile",
+        nargs="?",
+        action="store",
+        const="full",
+        type=str,
+        help="Run the profiler",
+        choices=["quick", "full"],
+    )
+    parser.add_argument(
+        "--sort",
+        action="store",
+        default="time",
+        type=str,
+        help="Sort the profiler output",
+        choices=["calls", "cumtime", "time"],
+    )
+    parser.add_argument(
+        "--seed",
+        action="store",
+        type=int,
+        default=100,
+        help="Seed for the environment",
+    )
     args = parser.parse_args()
 
     if args.profile:
-        perform_profiling()
+        perform_profiling(args.profile, args.sort, args.seed)
         exit()
 
-    mcts_samples()
--- match3tile/board.py
+++ match3tile/board.py
@@ -375,13 +375,13 @@ class Board:
         token1, token2 = self.array[cell1], self.array[cell2]
         
         # Early return for quickly identifiable actions - sorted by likelihood to save on function calls
-        # If the tokens have the same elements we CANNOT swap them
-        # If one of the tokens is a big bad we CAN swap them
         # If the tokens are both special tokens we CAN swap them
         if self.is_special(token1) and self.is_special(token2):
             return True
+        # If one of the tokens is a big bad we CAN swap them
         if self.is_big_bad(token1) or self.is_big_bad(token2):
             return True
+        # If the tokens have the same elements we CANNOT swap them
         if self.quick_get_token_element(token1) == self.quick_get_token_element(token2):
             return False
         
